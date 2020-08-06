a64460e0692b57962ef818fd255bd9900fce26c7
Siarhei Piashko <Siarhei.Piashko@seavus.com>
2020-08-06 15:49:09 +0300

    init

---
+++ consumer/src/test/java/com/spaishko/stringcloudstreamkafka/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spaishko.stringcloudstreamkafka.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
